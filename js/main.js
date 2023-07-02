489c98d795ff58e09230ac87ee0eeb77020d1ac5
Simon Kim <simonkim008@gmail.com>
2023-07-02 09:29:38 -0500

    completd r/p/s

--- js/main.js
+++ js/main.js
@@ -9,16 +9,30 @@ async function makeReq(){
 
   console.log(data);
   let l1 = (Math.ceil(Math.random() *3) )
-  l1 === 1 ? document.querySelector("#personStatus").textContent = 'rock' : Math.ceil(Math.random() *3) === 2 ? document.querySelector("#personStatus").textContent ='paper' : document.querySelector("#personStatus").textContent ='scissor' 
+  l1 === 1 ? document.querySelector("#personStatus").textContent = 'rock' : l1 === 2 ? document.querySelector("#personStatus").textContent ='paper' : document.querySelector("#personStatus").textContent ='scissor' 
   
    let l2 = (Math.ceil(Math.random() *3) )
-   l2 === 1 ? document.querySelector("#personOccupation").textContent = 'rock' : Math.ceil(Math.random() *3) === 2 ? document.querySelector("#personOccupation").textContent ='paper' : document.querySelector("#personOccupation").textContent ='scissor' 
-   if (l1 > l2){
-    console.log('player 1 wins')
-   }else if (l2 > l1){
-    console.log('player 2 wins')
-   }else{
-    console.log('draw')
-   }
+   l2 === 1 ? document.querySelector("#personOccupation").textContent = 'rock' : l2 === 2 ? document.querySelector("#personOccupation").textContent ='paper' : document.querySelector("#personOccupation").textContent ='scissor' 
    
+    if (l1 === 1 && l2 === 1){
+      document.querySelector("#personName").textContent = 'draw'
+    }else if ((l1 === 1) && (l2 === 2)){
+      document.querySelector("#personName").textContent = 'player 2 wins'
+    }else if ((l1 === 1) && (l2 === 3)){
+      document.querySelector("#personName").textContent = 'player 1 wins'
+    }else if ((l1 === 2) && (l2 === 1)){
+      document.querySelector("#personName").textContent = 'player 1 wins'
+    }else if ((l1 === 2) && (l2 === 2)){
+      document.querySelector("#personName").textContent = 'draw'
+    }else if ((l1 === 2) && (l2 === 3)){
+      document.querySelector("#personName").textContent = 'player 2 wins'
+    }else if ((l1 === 3) && (l2 === 1)){
+      document.querySelector("#personName").textContent = 'player 2 wins'
+    }else if ((l1 === 3) && (l2 === 2)){
+      document.querySelector("#personName").textContent = 'player 1 wins'
+    }else{
+      document.querySelector("#personName").textContent = 'draw'
+    }
+
+
 }
